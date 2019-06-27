e4d0b1fbea173a52a5b1f17568cc1cbd80f58733
Thanasis <thanasis.anthopoulos@ons.gov.uk>
2019-06-27 13:30:39 +0100

    Initial commit

--- scripts/main.py
+++ scripts/main.py
@@ -3,12 +3,14 @@ import pandas as pd
 from matplotlib import pyplot
 import random
 
+
 def plot(series_df, term='base_station', feeling_lucky=False):
     term = series_df.columns[random.randint(0, series_df.shape[0])] if feeling_lucky else term
     series = series_df[term] if not feeling_lucky else series_df[term]
     series.plot()
     pyplot.title(term)
     pyplot.show()
+
 
 series_df = pd.read_csv(os.path.join('data', 'timeseries_10k.csv')).transpose().reset_index()
 series_df.columns = series_df.iloc[0]
